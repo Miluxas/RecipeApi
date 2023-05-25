9859ebba7cde8fe385658b41088914c1b4233885
Milad <Milad@MILAD-PC>
2023-05-25 18:31:23 +0430

    Add project files.

---
+++ RecipeApi/Data/RecipeApiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RecipeApi.Recipe;
+
+namespace RecipeApi.Data
+{
+    public class RecipeApiContext : DbContext
+    {
+        public RecipeApiContext (DbContextOptions<RecipeApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RecipeApi.Recipe.Recipe> Recipe { get; set; } = default!;
+    }
+}
---
+++ RecipeApi/Recipe/Recipe.cs
@@ -0,0 +1,15 @@
+﻿namespace RecipeApi.Recipe
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Instructions { get; set; }
+        public int CookingTime { get; set; }
+        public int DifficultyLevel { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+    }
+}
